c1d9c6cc5440fb725a2656212122bffa170d62a2
jialu <806161729@qq.com>
2019-06-23 01:02:07 +0800

    touch

--- Memo.js
+++ Memo.js
@@ -216,97 +216,97 @@ function update() {
 
   $('.count').html((activeCount || 'No') + (activeCount > 1 ? ' items' : ' item'));
 
-  
+  //参考开源代码https://github.com/Orange1991/demos-in-sfg.name/tree/master/code/4_left_sliding_button_in_listview
   $(".line-scroll-wrapper").width($(".line-wrapper").width() + $(".line-btn-cancel").width() + $(".line-btn-delete").width());
   $(".line-normal-wrapper").width($(".line-wrapper").width());
   var lines = $(".line-normal-wrapper");
   var len = lines.length;
   var lastX, lastXForMobile;
-  // 用于记录被按下的对象
-  var pressedObj; // 当前左滑的对象
-  var lastLeftObj; // 上一个左滑的对象
-  // 用于记录按下的点
+  
+  var pressedObj; 
+  var lastLeftObj; 
+  
   var start;
-  // 网页在移动端运行时的监听
+  
   for (var i = 0; i < len; ++i) {
     lines[i].addEventListener('touchstart', function (e) {
       lastXForMobile = e.changedTouches[0].pageX;
-      pressedObj = this; // 记录被按下的对象 
-      // 记录开始按下时的点
+      pressedObj = this;  
+      
       var touches = event.touches[0];
       start = {
-        x: touches.pageX, // 横坐标
-        y: touches.pageY // 纵坐标
+        x: touches.pageX, 
+        y: touches.pageY 
       };
     });
     lines[i].addEventListener('touchmove', function (e) {
-      // 计算划动过程中x和y的变化量
+      
       var touches = event.touches[0];
       delta = {
         x: touches.pageX - start.x,
         y: touches.pageY - start.y
       };
-      // 横向位移大于纵向位移，阻止纵向滚动
+      
       if (Math.abs(delta.x) > Math.abs(delta.y)) {
         event.preventDefault();
       }
     });
     lines[i].addEventListener('touchend', function (e) {
-      if (lastLeftObj && pressedObj != lastLeftObj) { // 点击除当前左滑对象之外的任意其他位置
+      if (lastLeftObj && pressedObj != lastLeftObj) { 
         $(lastLeftObj).animate({
           marginLeft: "0"
-        }, 500); // 右滑
-        lastLeftObj = null; // 清空上一个左滑的对象
+        }, 500); 
+        lastLeftObj = null; 
       }
       var diffX = e.changedTouches[0].pageX - lastXForMobile;
       if (diffX < -150) {
         $(pressedObj).animate({
           marginLeft: "-132px"
-        }, 500); // 左滑
+        }, 500);
         lastLeftObj && lastLeftObj != pressedObj &&
           $(lastLeftObj).animate({
             marginLeft: "0"
-          }, 500); // 已经左滑状态的按钮右滑
-        lastLeftObj = pressedObj; // 记录上一个左滑的对象
+          }, 500); 
+        lastLeftObj = pressedObj; 
       } else if (diffX > 150) {
         if (pressedObj == lastLeftObj) {
           $(pressedObj).animate({
             marginLeft: "0"
-          }, 500); // 右滑
-          lastLeftObj = null; // 清空上一个左滑的对象
+          }, 500); 
+          lastLeftObj = null; 
         }
       }
     });
   }
-  // 网页在PC浏览器中运行时的监听
+ 
   for (var i = 0; i < len; ++i) {
     $(lines[i]).bind('mousedown', function (e) {
       lastX = e.clientX;
-      pressedObj = this; // 记录被按下的对象
+      pressedObj = this; 
     });
     $(lines[i]).bind('mouseup', function (e) {
-      if (lastLeftObj && pressedObj != lastLeftObj) { // 点击除当前左滑对象之外的任意其他位置
+      if (lastLeftObj && pressedObj != lastLeftObj) { 
         $(lastLeftObj).animate({
           marginLeft: "0"
-        }, 500); // 右滑
-        lastLeftObj = null; // 清空上一个左滑的对象
+        }, 500); 
+        lastLeftObj = null; 
       }
       var diffX = e.clientX - lastX;
       if (diffX < -150) {
         $(pressedObj).animate({
           marginLeft: "-132px"
-        }, 500); // 左滑
+        }, 500); 
         lastLeftObj && lastLeftObj != pressedObj &&
           $(lastLeftObj).animate({
             marginLeft: "0"
-          }, 500); // 已经左滑状态的按钮右滑
-        lastLeftObj = pressedObj; // 记录上一个左滑的对象
+          }, 500); 
+        lastLeftObj = pressedObj; 
       } else if (diffX > 150) {
         if (pressedObj == lastLeftObj) {
           $(pressedObj).animate({
             marginLeft: "0"
-          }, 500); // 右滑
-          lastLeftObj = null; // 清空上一个左滑的对象
+          }, 500); 
+          lastLeftObj = null; 
         }
       }
     });
